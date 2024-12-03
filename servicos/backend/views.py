93b8b296ff98147202c3c0d284940a05437cfe85
ryan <ryan.piinheiro@Hotmail.com>
2024-12-03 11:27:19 -0300

    aula 03/12 - construindo endpoints

--- servicos/backend/views.py
+++ servicos/backend/views.py
@@ -3,6 +3,12 @@ from rest_framework import viewsets
 from .models import Cliente
 from .serializers import *
 
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
+# from django.db.models import Count, Max, Min, Sum
+
+
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer 
@@ -10,3 +16,17 @@ class ClienteViewSet(viewsets.ModelViewSet):
 class ServicoViewSet(viewsets.ModelViewSet):
     queryset = Servico.objects.all()
     serializer_class = ServicoSerializer
+
+    @action(detail=False, methods=['get'])
+    def servicos_cliente(self, request):
+        cliente_id = request.query_params.get('cliente_id')
+        servicos = Servico.objects.filter(cliente_id=cliente_id)
+        serializer = ServicoSerializer(servicos, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['get'])
+    def servicos_pendentes(self, request):
+        cliente_id = request.query_params.get('cliente_id')
+        servicos = Servico.objects.filter(cliente_id=cliente_id, pago=False)
+        serializer = ServicoSerializer(servicos, many=True)
+        return Response(serializer.data)
--- servicos/servicos/urls.py
+++ servicos/servicos/urls.py
@@ -47,4 +47,5 @@ urlpatterns = [
     path('servicos_api/', include(router.urls)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', include(router.urls)),
 ]
